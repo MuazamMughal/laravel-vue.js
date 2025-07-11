cd0b4e9645548c7a19f0b2d44e0c58847a70c079
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-07-11 14:12:09 +0500

    booking calerndar

---
+++ job_Work/calendar_component/bookingCalendar.vue
@@ -0,0 +1,11 @@
+ <script setup>
+import { ref, computed, onMounted, watch } from 'vue'
+import dayjs from 'dayjs'
+import axios from 'axios'
+import ProceedArrow from './svg/ProceedArrow.vue'
+import ViewModalV2 from './ViewModalV2.vue';
+
+const props = defineProps({
+  openModal: Boolean,
+  data: Object,
+})
