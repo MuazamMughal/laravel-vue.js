6475ecf9aaaa9b24e6c22199df7857e484707ca5
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-07-12 14:13:19 +0500

    booking calerndar

--- job_Work/calendar_component/bookingCalendar.vue
+++ job_Work/calendar_component/bookingCalendar.vue
@@ -24,3 +24,15 @@ const timeSlots = ref([]);
 const loading = ref(false);
 const proceeding = ref(false);
 const error = ref('');
+
+
+
+// All days of the week (lower-case for easier comparison)
+const weekDays = [ 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+// This will hold the days that are NOT returned in `working_days` from the API
+const offDays = ref([]);
+
+const daysInMonth = computed(() => {
+  const start = selectedDate.value.startOf('month')
+  const end = selectedDate.value.endOf('month')
+  const days = []
