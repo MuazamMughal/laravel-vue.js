ee6a78ec1c91eae47fbc7a96b96adebca5a041e5
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-07-31 17:50:01 +0500

    vue sw updated

---
+++ job_Work/Laravel-PWA/Service Worker/sw.vue
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'offline-tasks-v1';
+const urlsToCache = ['/', '/static/js/bundle.js', '/static/css/main.css', '/icon-192.png', '/icon-512.png'];
+
+// Install: Cache the App Shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
