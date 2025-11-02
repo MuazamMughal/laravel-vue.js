2aebb2b33f1604c9c3005a4ede9ea4a68c429994
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-11-03 02:59:29 +0500

    E transformer

--- job_Work/event_audiance_invite/Etransformer.php
+++ job_Work/event_audiance_invite/Etransformer.php
@@ -5,4 +5,10 @@ namespace App\Transformers\Admin\Events;
 class EventTransformer extends TransformerAbstract
 {
     protected $type;
-      protected array $availableIncludes = ['host', 'hosts', 'expert', 'partnerChannel', 'categories', 'attendees', 'attachments', 'links', 'ticket', 'channel', 'tags', 'eventAudiences', 'vonageSession', 'participants', 'files', 'userEventActivities', 'eventPartnerChannels', 'seriesEvents', 'eventRecordings', 'handRaisedUsers'];
+      protected array $availableIncludes = [ 'eventAudiences'];
+        protected $graceStartMin = 10;
+    protected $graceEndMin = 0;
+    public function __construct($type)
+    {
+        $this->type = $type;
+    }
