1ed14218936c68e08490950b0a91ab3ba0678f2e
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-11-07 03:13:41 +0500

    Helper Fuction

--- job_Work/event_audiance_invite/helperfunction.php
+++ job_Work/event_audiance_invite/helperfunction.php
@@ -1,4 +1,9 @@
 if (!function_exists('prepareEditData')) {
     function prepareEditData($event, $type, $typeBaseUrl)
     {
-        
+          $event->load([ 'eventAudiences'])
+
+            $eventData = fractal(
+            $event,
+            new \App\Transformers\Admin\Events\EventTransformer(EventTypeEnum::EVENT)
+        )->parseIncludes(['hosts', 'categories', 'attendees', 'attachments', 'links', 'ticket', 'channel', 'tags', 'vonageSession', 'participants', 'eventPartnerChannels', 'eventRecordings', 'eventAudiences']);
