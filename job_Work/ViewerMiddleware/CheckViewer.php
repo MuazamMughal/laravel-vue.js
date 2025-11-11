5b36bad99d2ded98baa9ebecfa7037c46e7a80a0
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-11-12 04:02:14 +0500

    Check viewer Middleware

--- job_Work/ViewerMiddleware/CheckViewer.php
+++ job_Work/ViewerMiddleware/CheckViewer.php
@@ -6,3 +6,15 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
+
+class CheckIsViewer
+{
+    
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        
+        // Check if user has viewer role
+        if ($user && $user->role === 'viewer') {
+            return redirect("/home")->with('error', 'You do not have permission to access this page.');
+        }
