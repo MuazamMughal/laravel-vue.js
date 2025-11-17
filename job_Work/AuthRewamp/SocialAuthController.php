8d185ce196b287be8a51512c4a6abfb65297de59
Muazam Mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-11-18 04:17:49 +0500

    SocilRegister Controller

--- job_Work/AuthRewamp/SocialAuthController.php
+++ job_Work/AuthRewamp/SocialAuthController.php
@@ -14,3 +14,45 @@ use Illuminate\Support\Facades\Session;
 use App\Models\ChannelCategory;
 use App\Models\ExpertCategory;
 use Illuminate\Support\Str;
+
+class SocialRegisterController extends Controller
+{
+    public function index(): Response
+    {
+        $skills = Skill::all();
+        $channelCategories = ChannelCategory::all();
+        $expertCategories = ExpertCategory::all();
+        return Inertia::render('Auth/Register', [
+            'skills' => $skills,
+            'channelCategories' => $channelCategories,
+            'expertCategories' => $expertCategories
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $profile = $request->profile ?? '';
+        
+        $rules = [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'profile' => 'required|in:expert,viewer,channel',
+        ];
+
+        if ($profile === 'expert') {
+            $rules = array_merge($rules, [
+                'category_id' => ['required'],
+                'linkedin_url' => 'required|string|' . url_pattern(),
+            ]);
+        } elseif ($profile === 'channel') {
+            $rules = array_merge($rules, [
+                'category_id' => 'required',
+                'state_id' => 'required',
+                'logo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'cover_photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+        }
+
+        $request->validate($rules);
+
+        $user = User::where('email', $request->email)->first();
